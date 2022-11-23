ce8fbda0967da3be54f6366fc51575f3e3bb4e12
ezivex <35563094+ezivex@users.noreply.github.com>
2022-11-23 22:42:48 +0100

    Update App.js

--- komunikacja-react/src/App.js
+++ komunikacja-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          1st deploy on github page is successfull
         </p>
         <a
           className="App-link"
