3e0303a62e2fa39079ae6c4bb6fd31ad3a617774
ezivex <35563094+ezivex@users.noreply.github.com>
2022-11-27 15:26:01 +0100

    Navbar

--- komunikacja-react/src/App.js
+++ komunikacja-react/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
+</style>
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Testowy text elo elo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
     </div>
   );
 }
---
+++ komunikacja-react/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+    <nav className="Navbar">
+        <div className="logo">
+            <p>LOGO BUSY.PL</p>
+        </div>
+        <div className="links">
+            <a href="/">KIEROWCA</a>
+            <a href="/">DYSPOZYTOR</a>
+            <a href="/">ADMIN</a>
+        </div>
+    </nav> );
+}
+ 
+export default Navbar;
