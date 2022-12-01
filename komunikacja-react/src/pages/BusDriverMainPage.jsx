deae240e0ea8f3490c5705347f9310f6ef6ed7d3
kamilpawlica <kamilpawlica99@gmail.com>
2022-12-01 19:51:08 +0100

    css_config

--- komunikacja-react/src/pages/BusDriverMainPage.jsx
+++ komunikacja-react/src/pages/BusDriverMainPage.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function BusDriverMainPage() {
     return (
-        <h1>this is the BusDriverMainPage</h1>
+        <h1 className='Title'>this is the BusDriverMainPage
+        
+        </h1>
     );
 }
 
