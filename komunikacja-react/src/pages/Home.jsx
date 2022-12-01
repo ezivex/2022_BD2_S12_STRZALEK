c4ae66d21a54246f5b722835e9e0bd79c63d47c2
ezivex <35563094+ezivex@users.noreply.github.com>
2022-12-01 20:52:52 +0100

    Update Home.jsx

--- komunikacja-react/src/pages/Home.jsx
+++ komunikacja-react/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Home() {
     return (
         <div>
-        <h1>this is the homepage</h1>
+        <h1>this is the homepage test for everyone</h1>
         </div>
     );
 }
