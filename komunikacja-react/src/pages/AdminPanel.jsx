fd1075f85119baa0c6a5613ab27f08dc7324acbc
lukaszwolny <lukawol372@student.polsl.pl>
2022-12-01 20:38:25 +0100

    zmiana css admin
    testy kolejne pozdro

--- komunikacja-react/src/pages/AdminPanel.jsx
+++ komunikacja-react/src/pages/AdminPanel.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function AdminPanel() {
     return (
-        <h1 className='titleAdmin'>this is the AdminPanel</h1>
+        <div className="contanerAdmin">
+            <h1 className='titleAdmin'>this is the AdminPanel</h1>
+            <div className="main">
+                <p className="admnintext1">siema sime amie</p>
+            </div>
+        </div>
     );
 }
 
