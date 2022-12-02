02129e15feedd7e8ea60f97a01fc86e13f0105fa
kamilpawlica <kamilpawlica99@gmail.com>
2022-12-02 20:23:39 +0100

    Update BusDriverDyspozycja.jsx

--- komunikacja-react/src/pages/BusDriverDyspozycja.jsx
+++ komunikacja-react/src/pages/BusDriverDyspozycja.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-
+import {Link } from "react-router-dom";
 function BusDriverDyspozycja() {
     return (
-        <body id="bodyDyspozycja">
-            <div className="containerDyspozycja">
+        
+        <div className="containerDyspozycja">
+            <div className="logoGrafik">
+                    LOGO BUSY.PL
+                    <Link to="/BusDriverMainPage">
+                    <button className="backButton"> POWRÓT </button>
+                    </Link>
+                </div>
 
 
-            </div>
-        </body>
+
+
+
+        </div>
+
+        
     );
 }
 
