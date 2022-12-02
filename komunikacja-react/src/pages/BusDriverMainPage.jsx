cdb1272984296d832e4470d997a16c61155a72cf
kamilpawlica <kamilpawlica99@gmail.com>
2022-12-02 09:20:43 +0100

    update_css

--- komunikacja-react/src/pages/BusDriverMainPage.jsx
+++ komunikacja-react/src/pages/BusDriverMainPage.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 
 function BusDriverMainPage() {
     return (
-        
-        <div id='container'>
-            <h1 className='titleBusDriverMainPage'>LOGO BUSY.PL</h1>
-        
-        
-        
-        
-        
-        
-        
-        </div>
+        <body id="bodyBusDriverMainPage">
+        <div className='containerBusDriverMainPage'>
+
+            <div className="logoBusDriverMainPage">
+            <h1>LOGO BUSY.PL</h1>
+            </div>
+
+            <div className="navbarBusDriverMainPage">
+            <h2>Witaj, kierowco</h2>
+            </div>
+
+            <div className="menuBusDriverMainPage">
+            <button className="leftButtonMainPage">DYSPOZYCJA</button>
+            
+            <button className="rightButtonMainPage">SPRAWDŹ GRAFIK</button>
+            
+            </div>
+            
+
+            <div className="footerBusDriverMainPage">
+            <p className="pbuttonLogout"><button className="buttonLogout">WYLOGUJ</button></p>
+            <p className="copyright">2022 &copy;COPYRIGHT</p>
+            </div>
+         </div>
+        </body>
 
     );
 }
