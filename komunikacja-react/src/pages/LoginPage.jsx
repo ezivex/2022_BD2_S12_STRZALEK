92ce9909f0db50bafed15b58595b571d917b69a8
ezivex <35563094+ezivex@users.noreply.github.com>
2022-12-03 12:05:51 +0100

    loginPage
    logging now available

--- komunikacja-react/src/pages/LoginPage.jsx
+++ komunikacja-react/src/pages/LoginPage.jsx
@@ -1,27 +1,98 @@
 import React from 'react';
+import { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+
 
 function LoginPage() {
+
+
+const [errMsg, setErrMsg] = useState({});
+const [isSubmitted, setIsSubmitted] = useState(false);
+const navigate = useNavigate();
+// Users login info
+
+const loginCred = [
+    {
+      username: 'admin1',
+      password: 'admin',
+    },
+    {
+      username: 'kierowca1',
+      password: 'kierowca',
+    },
+    {
+      username: 'dyspozytor1',
+      password: 'dyspozytor',
+    }
+];
+
+const errors = {
+    uname: 'invalid username!!!',
+    pass: 'invalid password!!!'
+};
+
+const handleSubmit = (e)=> {
+    e.preventDefault(); // prevent page reload
+
+    let {uname, pass} = document.forms[0];
+    // find user login info
+    const userData = loginCred.find((user) => user.username === uname.value);
+    // compare
+    if (userData){
+        if (userData.password !== pass.value) {
+            // Invalid password
+            setErrMsg({ name: "pass", message: errors.pass });
+          } else {
+            setIsSubmitted(true);
+            if(userData.username==='admin1'){
+                navigate('/AdminPanel');
+               }
+               else if(userData.username==='kierowca1'){
+                navigate('/BusDriverMainPage');
+               }else if(userData.username==='dyspozytor1'){
+                navigate('/DispacherPanel');
+               }
+          }
+        } else {
+            // Username not found
+            setErrMsg({ name: "uname", message: errors.uname });
+          }
+             
+};
+
+//generate error's message
+const renderErrMsg = (name) =>
+    name === errMsg.name && (<div className='error'>{errMsg.message}</div>);
+
+
+const renderForm = (
+  
+         <form className='loging' onSubmit={handleSubmit}>
+             <div className="input-container">
+                 <label>Username </label>
+                 <input type="text" name="uname" required />
+                     {renderErrMsg("uname")}
+             </div>
+             <div className="input-container">
+                 <label>Password </label>
+                 <input type="password" name="pass" required />
+                 {renderErrMsg("pass")} 
+             </div>
+             <div className="button-container">
+             <input type="submit" />
+             </div>
+         </form>
+ 
+);
+
     return (
         <div className="LoginContainer">
-           <div className="logoLogin"><p>logo busy.pl</p></div>
-           <div className="loginForm">
-                <h2 className="loginTitle">Zaloguj sie</h2>
-                <form className='loging'>
-                    <div className="input-container">
-                        <label>Username </label>
-                        <input type="text" name="uname" required />
-                            {/* {renderErrorMessage("uname")} */}
-                    </div>
-                    <div className="input-container">
-                        <label>Password </label>
-                        <input type="password" name="pass" required />
-                        {/* {renderErrorMessage("pass")} */}
-                    </div>
-                    <div className="button-container">
-                    <input type="submit" />
-                    </div>
-                </form>
-            </div> 
+             <div className="logoLogin"><p>logo busy.pl</p></div>
+             
+        <div className="loginForm">
+             <h2 className="loginTitle">Zaloguj sie</h2>
+             {isSubmitted ? console.log('done') : renderForm}
+         </div>
         </div>
     );
 }
