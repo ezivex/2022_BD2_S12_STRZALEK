f978fbb50ea554e7f4db2e8bef204d04257afc33
lukaszwolny <lukawol372@student.polsl.pl>
2022-12-04 18:07:53 +0100

    adminpanelprzyst

--- komunikacja-react/src/pages/AdminPanelprzyst.jsx
+++ komunikacja-react/src/pages/AdminPanelprzyst.jsx
@@ -3,7 +3,28 @@ import {Link } from "react-router-dom";
 function AdminPanelprzyst() {
     return (
         <div className="containerAdminPanelprzyst">
-            <p>to jest przystanek</p>
+            <div className="powrotAdminPanelprzyst">
+                <button className="przyciskpowrotAdminPanelprzyst">powrót</button>
+            </div>
+            <div className="stronaAdminPanelprzyst">
+                <div className="listaprzystAdminPanelprzyst">
+                    <p className="przystanktytulAdminPanelprzyst">Przystanki</p>
+                    <ul className="ulprzystnkiAdminPanelprzyst">
+                        <li className="liprzystankiAdminPanelprzyst">przystanek1</li>
+                        <li className="liprzystankiAdminPanelprzyst">przystanek2</li>
+                        <li className="liprzystankiAdminPanelprzyst">przystanek3</li>
+                    </ul>
+                </div>
+                <div className="dodajprzystAdminPanelprzyst">
+                    <input className="inputAdminPanelprzyst"></input>
+                    <button className="dodAdminPanelprzyst">dodaj przystanek</button>
+                    <input className="inputAdminPanelprzyst"></input>
+                    <button className="usuAdminPanelprzyst">usuń przystanek</button>
+                </div>
+            </div>
+            <div className="stopkaAdminPanel">
+                <p className="copyrightAdminPanel">2022 @COPYRIGHT</p>
+            </div>
         </div>
     );
 }
