b5bb4e757a50d24f05b05de31334c9b9b7e8e72b
kamilpawlica <kamilpawlica99@gmail.com>
2022-12-05 14:12:21 +0100

    Updates

---
+++ precache-manifest.f33c5833f9d6ef162cc0a4df0553a559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5b1130585dfb2b08238",
+    "url": "/2022_BD2_S12_STRZALEK/static/css/main.7b287bc7.chunk.css"
+  },
+  {
+    "revision": "d5b1130585dfb2b08238",
+    "url": "/2022_BD2_S12_STRZALEK/static/js/main.d5b11305.chunk.js"
+  },
+  {
+    "revision": "d9a9361b93564cda0aea",
+    "url": "/2022_BD2_S12_STRZALEK/static/js/1.d9a9361b.chunk.js"
+  },
+  {
+    "revision": "6be5c94b9bc759ebcf7b",
+    "url": "/2022_BD2_S12_STRZALEK/static/js/runtime~main.6be5c94b.js"
+  },
+  {
+    "revision": "dc0c030650f8cee466199b5d17cc88ff",
+    "url": "/2022_BD2_S12_STRZALEK/static/media/btn_przystanki.dc0c0306.svg"
+  },
+  {
+    "revision": "6aeaacab73ba9be013247822c5aed8c0",
+    "url": "/2022_BD2_S12_STRZALEK/static/media/btn_linie.6aeaacab.svg"
+  },
+  {
+    "revision": "4c03ac3df41de1ef80c17a97e5a39ca6",
+    "url": "/2022_BD2_S12_STRZALEK/index.html"
+  }
+];
