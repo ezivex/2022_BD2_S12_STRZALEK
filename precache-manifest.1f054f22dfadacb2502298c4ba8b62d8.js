7622ef4ae9dc3bac47514eda4e1888539546a18b
lukaszwolny <lukawol372@student.polsl.pl>
2022-12-05 16:35:20 +0100

    Updates

---
+++ precache-manifest.1f054f22dfadacb2502298c4ba8b62d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "143fc6b003bd0f66aad3",
+    "url": "/2022_BD2_S12_STRZALEK/static/css/main.8bf8ff8d.chunk.css"
+  },
+  {
+    "revision": "143fc6b003bd0f66aad3",
+    "url": "/2022_BD2_S12_STRZALEK/static/js/main.143fc6b0.chunk.js"
+  },
+  {
+    "revision": "9b86f8e2322cb3daf97a",
+    "url": "/2022_BD2_S12_STRZALEK/static/js/1.9b86f8e2.chunk.js"
+  },
+  {
+    "revision": "6be5c94b9bc759ebcf7b",
+    "url": "/2022_BD2_S12_STRZALEK/static/js/runtime~main.6be5c94b.js"
+  },
+  {
+    "revision": "dc0c030650f8cee466199b5d17cc88ff",
+    "url": "/2022_BD2_S12_STRZALEK/static/media/btn_przystanki.dc0c0306.svg"
+  },
+  {
+    "revision": "6aeaacab73ba9be013247822c5aed8c0",
+    "url": "/2022_BD2_S12_STRZALEK/static/media/btn_linie.6aeaacab.svg"
+  },
+  {
+    "revision": "ea16e6e02a91854af6cb1a761eef5f3b",
+    "url": "/2022_BD2_S12_STRZALEK/index.html"
+  }
+];
