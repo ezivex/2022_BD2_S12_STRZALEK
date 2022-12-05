897496e230afd35f750db27c09a0f34659264be5
todudek <todudek@gmail.com>
2022-12-05 11:56:07 +0100

    Update DispatcherMainPanel.jsx

--- komunikacja-react/src/pages/DispatcherMainPanel.jsx
+++ komunikacja-react/src/pages/DispatcherMainPanel.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-
-function DispatcherPanel() {
+import {Link } from "react-router-dom";
+function DispatcherMainPanel() {
     return (
-        <h1 className='titleDispatcherPanel'>nie daje brzydkich slow</h1>
+
+        <div className='containerDispatcherMainPanel'>
+
+        <div className="logoDispatcherMainPanel">
+                <p className="logoMainPage">LOGO BUSY.PL</p>
+            </div>
+
+        <div className="navbarDispatcherMainPanel">
+             <h2>PANEL DYSPOZYTORA</h2>
+        </div>
+
+        <div className="menuDispatcherMainPanel">
+                <Link to="/DispatcherZarzadzanieKursami">
+                <button className="leftButtonMainPanel">ZARZĄDZANIE KURSAMI</button>
+                </Link>
+    
+                <Link to="/DispatcherEdycjaKursow">
+                <button className="rightButtonMainPanel">EDYCJA KURSÓW</button>
+                </Link>
+        </div>
+
+        <footer className="PageFooter buttonSettings"> 
+                
+                <button className="buttonLogout"><Link to="/LoginPage"> WYLOGUJ </Link></button>
+                <p>2022 BD2 Projekt</p>
+                
+        </footer>
+
+        </div>
     );
 }
 
-export default DispatcherPanel;
+export default DispatcherMainPanel;
