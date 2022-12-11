004de749904f3615cc8f83c51fc28cab655eeda7
kamilpawlica <kamilpawlica99@gmail.com>
2022-12-11 19:02:22 +0100

    api testing

--- komunikacja-react/server/database.sql
+++ komunikacja-react/server/database.sql
@@ -1,6 +1,21 @@
 CREATE DATABASE perntodo;
 
-CREATE TABLE todo(
-  todo_id SERIAL PRIMARY KEY,
-  description VARCHAR(255)
+-- CREATE TABLE public.autobusy
+-- (
+--     id_bus SERIAL PRIMARY KEY,
+--     rejestracja VARCHAR(10) NOT NULL,
+--     marka VARCHAR(50) NOT NULL,
+--     sprawnosc boolean NOT NULL,
+--     rodzaj_autobusy integer NOT NULL
+-- );
+
+
+CREATE TABLE IF NOT EXISTS public.autobusy
+(
+    id_bus integer[] NOT NULL,
+    rejestracja character varying(10)[] COLLATE pg_catalog."default" NOT NULL,
+    marka character varying(50)[] COLLATE pg_catalog."default" NOT NULL,
+    sprawnosc boolean[] NOT NULL,
+    rodzaj_autobusy integer[] NOT NULL,
+    CONSTRAINT test_pkey PRIMARY KEY (id_bus)
 );
--- komunikacja-react/server/index.js
+++ komunikacja-react/server/index.js
@@ -11,12 +11,12 @@ app.use(express.json()); //req.body
 
 //create a todo
 
-app.post("/todos", async (req, res) => {
+app.post("/autobusy", async (req, res) => {
   try {
-    const { description } = req.body;
+    const { rejestracja,marka,sprawnosc,rodzaj_autobusy } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1) RETURNING *",
-      [description]
+      "INSERT INTO autobusy (rejestracja,marka,sprawnosc,rodzaj_autobusy) VALUES($1,$2,$3,$4) RETURNING *",
+      [rejestracja,marka,sprawnosc,rodzaj_autobusy]
     );
 
     res.json(newTodo.rows[0]);
@@ -25,22 +25,63 @@ app.post("/todos", async (req, res) => {
   }
 });
 
-//get all
+//get all todos
+
+app.get("/autobusy", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM autobusy ORDER BY id_bus ASC");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 
+//get a specific todo
+app.get("/autobusy/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM autobusy WHERE id_bus = $1", [
+      id
+    ]);
 
-
-//get
-
-
-
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //update
+app.put("/autobusy/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rejestracja,marka,sprawnosc,rodzaj_autobusy } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE autobusy SET rejestracja = $1, marka = $2, sprawnosc = $3, rodzaj_autobusy = $4  WHERE id_bus = $5",
+      [rejestracja,marka,sprawnosc,rodzaj_autobusy,id]
+    );
 
-
-
+    res.json("Autobusy was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //delete
+
+app.delete("/autobusy/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM autobusy WHERE id_bus = $1", [
+      id
+    ]);
+    res.json("Bus was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//check if server works
 app.listen(5000, () => {
     console.log("Server has started on port 5000")
 });
