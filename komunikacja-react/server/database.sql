34316cc42604cbbd1b78e751fef98f19b8ce8d4f
kamilpawlica <kamilpawlica99@gmail.com>
2022-12-11 19:29:58 +0100

    api_testing

--- komunikacja-react/server/database.sql
+++ komunikacja-react/server/database.sql
@@ -12,10 +12,10 @@ CREATE DATABASE perntodo;
 
 CREATE TABLE IF NOT EXISTS public.autobusy
 (
-    id_bus integer[] NOT NULL,
-    rejestracja character varying(10)[] COLLATE pg_catalog."default" NOT NULL,
-    marka character varying(50)[] COLLATE pg_catalog."default" NOT NULL,
-    sprawnosc boolean[] NOT NULL,
-    rodzaj_autobusy integer[] NOT NULL,
-    CONSTRAINT test_pkey PRIMARY KEY (id_bus)
+    id_bus SERIAL PRIMARY KEY,
+    rejestracja character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    marka character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    sprawnosc boolean NOT NULL,
+    rodzaj_autobusy integer NOT NULL
 );
+
