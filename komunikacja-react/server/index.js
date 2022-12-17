2ecc98a9d1959348a95d8fcde1b93d8b2bcb420d
kamilpawlica <kamilpawlica99@gmail.com>
2022-12-17 21:00:59 +0100

    apitesting

--- komunikacja-react/server/index.js
+++ komunikacja-react/server/index.js
@@ -3,13 +3,17 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-//middleware
+
 app.use(cors());
-app.use(express.json()); //req.body
+app.use(express.json()); 
 
 //ROUTES//
 
-//create a todo
+//------------------------------------- -------------------------------------  ADMIN PANEL ------------------------------------- ------------------------------------- //
+
+
+// ------------------------------------- AUTOBUSY -------------------------------------  //
+//ADD AUTOBUS 
 
 app.post("/autobusy", async (req, res) => {
   try {
@@ -25,7 +29,26 @@ app.post("/autobusy", async (req, res) => {
   }
 });
 
-//get all todos
+
+//ADD KURS 
+
+// app.post("/kurs", async (req, res) => {
+//   try {
+//     const { nazwaLini_k,rbus_k,czas_odjazdu } = req.body;
+//     const newTodo = await pool.query(
+//       "INSERT INTO kurs (nazwaLini_k,rbus_k,czas_odjazdu) VALUES($1,$2,$3) RETURNING *",
+//       [nazwaLini_k,rbus_k,czas_odjazdu]
+//     );
+
+//     res.json(newTodo.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+
+
+//GET ALL AUTOBUSY
 
 app.get("/autobusy", async (req, res) => {
   try {
@@ -37,7 +60,7 @@ app.get("/autobusy", async (req, res) => {
 });
 
 
-//get a specific todo
+//GET AUTOBUS
 app.get("/autobusy/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -51,7 +74,7 @@ app.get("/autobusy/:id", async (req, res) => {
   }
 });
 
-//update
+// UPDATE AUTOBUS
 app.put("/autobusy/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -67,7 +90,7 @@ app.put("/autobusy/:id", async (req, res) => {
   }
 });
 
-//delete
+//DELETE AUTOBUS
 
 app.delete("/autobusy/:id", async (req, res) => {
   try {
@@ -81,8 +104,130 @@ app.delete("/autobusy/:id", async (req, res) => {
   }
 });
 
-//check if server works
-app.listen(5000, () => {
-    console.log("Server has started on port 5000")
+
+// ------------------------------------- END OF AUTOBUSY -------------------------------------  //
+
+
+
+// ------------------------------------- PRZYSTANKI -------------------------------------  //
+
+
+//ADD PRZYSTANEK
+app.post("/przystanki", async (req, res) => {
+  try {
+    const { nazwaprzystanku } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO przystanki (nazwaprzystanku) VALUES($1) RETURNING *",
+      [nazwaprzystanku]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
+
+//GET ALL PRZYSTANKI
+
+app.get("/przystanki", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM przystanki ORDER BY id_przystanku ASC");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+//DELETE PRZYSTANEK
+
+app.delete("/przystanki/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM przystanki WHERE id_przystanku = $1", [
+      id
+    ]);
+    res.json("Przystanek was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// ------------------------------------- END OF PRZYSTANKI -------------------------------------  //
+
+// ------------------------------------- LINIA -------------------------------------  //
+
+
+//TODO
+
+
+
+// ------------------------------------- END OF LINIA -------------------------------------  //
+// ------------------------------------- KURSY -------------------------------------  //
+
+
+
+//TODO //testowanie
+
+
+//ADD KURS
+app.post("/kurs", async (req, res) => {
+  try {
+    const { nazwaLini_k,rbus_k,czas_odjazdu } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO kurs (nazwaLini_k,rbus_k,czas_odjazdu) VALUES($1,$2,$3) RETURNING *",
+      [ nazwaLini_k,rbus_k,czas_odjazdu ]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+//GET ALL KURSY
+
+app.get("/kurs", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM kurs ORDER BY id_kurs ASC");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+//DELETE KURS
+
+app.delete("/kurs/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM kurs WHERE id_kurs = $1", [
+      id
+    ]);
+    res.json("Kurs was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// ------------------------------------- END OF KURSY -------------------------------------  //
+
+
+
+
+
+
+
+//------------------------------------- ------------------------------------- END OF ADMIN PANEL ------------------------------------- ------------------------------------- //
+
+
+
+
+
+// CHECK IF SERVER WORKS
+app.listen(5000, () => {
+  console.log("Server has started on port 5000")
+});
--- komunikacja-react/server/komunikacjav11.sql
+++ komunikacja-react/server/komunikacjav11.sql
@@ -3,12 +3,12 @@
 BEGIN;
 
 
-CREATE TABLE IF NOT EXISTS public."Linia"
+CREATE TABLE IF NOT EXISTS public.linia
 (
     id_linii serial NOT NULL,
-    "nazwaLinii" character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    nazwaLinii character varying(10) COLLATE pg_catalog."default" NOT NULL,
     typ_linii integer NOT NULL,
-    CONSTRAINT "Linia_pkey" PRIMARY KEY (id_linii)
+    CONSTRAINT Linia_pkey PRIMARY KEY (id_linii)
 );
 
 CREATE TABLE IF NOT EXISTS public.autobusy
@@ -30,17 +30,17 @@ CREATE TABLE IF NOT EXISTS public.dni_tygodnia
 
 CREATE TABLE IF NOT EXISTS public.dostepnosci_kierowcow
 (
-    "id_dostKier" serial NOT NULL,
+    id_dostKier serial NOT NULL,
     id_uzytk integer NOT NULL,
     dzien_tyg integer NOT NULL,
     rodzaj_zmiany integer NOT NULL,
-    CONSTRAINT "dostKierowcy" PRIMARY KEY ("id_dostKier")
+    CONSTRAINT dostKierowcy PRIMARY KEY (id_dostKier)
 );
 
 CREATE TABLE IF NOT EXISTS public.kurs
 (
     id_kurs serial NOT NULL,
-    "nazwaLini_k" integer NOT NULL,
+    nazwaLini_k integer NOT NULL,
     rbus_k integer NOT NULL,
     czas_odjazdu integer NOT NULL,
     CONSTRAINT kurs_pkey PRIMARY KEY (id_kurs)
@@ -51,23 +51,23 @@ CREATE TABLE IF NOT EXISTS public.kurs_realizacja
     id_realizacji serial NOT NULL,
     id_kursu integer NOT NULL,
     dzien_rel integer NOT NULL,
-    "id_kierowcyRel" integer NOT NULL,
-    "id_busuRel" integer NOT NULL,
+    id_kierowcyRel integer NOT NULL,
+    id_busuRel integer NOT NULL,
     CONSTRAINT kurs_realizacja_pkey PRIMARY KEY (id_realizacji)
 );
 
-CREATE TABLE IF NOT EXISTS public."przystanekwLinii"
+CREATE TABLE IF NOT EXISTS public.przystanekwLinii
 (
-    "id_przystLin" serial NOT NULL,
+    id_przystLin serial NOT NULL,
     linia integer NOT NULL,
     przystanek_id integer NOT NULL,
-    CONSTRAINT "przystanekwLinii_pkey" PRIMARY KEY ("id_przystLin")
+    CONSTRAINT przystanekwLinii_pkey PRIMARY KEY (id_przystLin)
 );
 
-CREATE TABLE IF NOT EXISTS public."Przystanki"
+CREATE TABLE IF NOT EXISTS public.przystanki
 (
     id_przystanku serial NOT NULL,
-    "nazwaPrzystanku" character varying(35) COLLATE pg_catalog."default" NOT NULL,
+    nazwaprzystanku character varying(35) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT id_przystanku_pkey PRIMARY KEY (id_przystanku)
 );
 
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS public.raport
 CREATE TABLE IF NOT EXISTS public.rodzaj_autobusu
 (
     id_rodzaj serial NOT NULL,
-    "nazwaRodzaj" character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    nazwaRodzaj character varying(50) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT rodzaj_autobusu_pkey PRIMARY KEY (id_rodzaj)
 );
 
@@ -104,7 +104,7 @@ CREATE TABLE IF NOT EXISTS public.rozklad_jazdy
     CONSTRAINT rozklad_jazdy_pkey PRIMARY KEY (id_rj)
 );
 
-CREATE TABLE IF NOT EXISTS public."Typ_linii"
+CREATE TABLE IF NOT EXISTS public.typ_linii
 (
     id_typ_linii serial NOT NULL,
     nazwa_typu character varying(30) COLLATE pg_catalog."default" NOT NULL,
@@ -113,9 +113,9 @@ CREATE TABLE IF NOT EXISTS public."Typ_linii"
 
 CREATE TABLE IF NOT EXISTS public.typ_stanowiska
 (
-    "id_typStanowiska" serial NOT NULL,
+    id_typStanowiska serial NOT NULL,
     nazwa_stanowiska character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT typ_stanowiska_pkey PRIMARY KEY ("id_typStanowiska")
+    CONSTRAINT typ_stanowiska_pkey PRIMARY KEY (id_typStanowiska)
 );
 
 CREATE TABLE IF NOT EXISTS public.uzytkownicy
@@ -124,22 +124,22 @@ CREATE TABLE IF NOT EXISTS public.uzytkownicy
     imie character varying(30) COLLATE pg_catalog."default",
     nazwisko character varying(30) COLLATE pg_catalog."default",
     id_stanowisko integer NOT NULL,
-    ulica "char",
-    miasto "char",
+    ulica char,
+    miasto char,
     wiek integer,
     CONSTRAINT uzytkownicy_pkey PRIMARY KEY (id_uzytkownik)
 );
 
-ALTER TABLE IF EXISTS public."Linia"
-    ADD CONSTRAINT "typ_linii_FK" FOREIGN KEY (typ_linii)
-    REFERENCES public."Typ_linii" (id_typ_linii) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.linia
+    ADD CONSTRAINT typ_linii_FK FOREIGN KEY (typ_linii)
+    REFERENCES public.Typ_linii (id_typ_linii) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.autobusy
-    ADD CONSTRAINT "rodzajautobusu_bus_FK" FOREIGN KEY (rodzaj_autobusy)
+    ADD CONSTRAINT rodzajautobusu_bus_FK FOREIGN KEY (rodzaj_autobusy)
     REFERENCES public.rodzaj_autobusu (id_rodzaj) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -147,7 +147,7 @@ ALTER TABLE IF EXISTS public.autobusy
 
 
 ALTER TABLE IF EXISTS public.dostepnosci_kierowcow
-    ADD CONSTRAINT "dzienTyg_FK" FOREIGN KEY (dzien_tyg)
+    ADD CONSTRAINT dzienTyg_FK FOREIGN KEY (dzien_tyg)
     REFERENCES public.dni_tygodnia (id_dnityg) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -155,7 +155,7 @@ ALTER TABLE IF EXISTS public.dostepnosci_kierowcow
 
 
 ALTER TABLE IF EXISTS public.dostepnosci_kierowcow
-    ADD CONSTRAINT "idUzytk_FK" FOREIGN KEY (id_uzytk)
+    ADD CONSTRAINT idUzytk_FK FOREIGN KEY (id_uzytk)
     REFERENCES public.uzytkownicy (id_uzytkownik) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -163,7 +163,7 @@ ALTER TABLE IF EXISTS public.dostepnosci_kierowcow
 
 
 ALTER TABLE IF EXISTS public.dostepnosci_kierowcow
-    ADD CONSTRAINT "rodzajZmiany_FK" FOREIGN KEY (rodzaj_zmiany)
+    ADD CONSTRAINT rodzajZmiany_FK FOREIGN KEY (rodzaj_zmiany)
     REFERENCES public.rodzaj_zmiany (id_zmiany) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -171,15 +171,15 @@ ALTER TABLE IF EXISTS public.dostepnosci_kierowcow
 
 
 ALTER TABLE IF EXISTS public.kurs
-    ADD CONSTRAINT "nazwaLiniii_FK" FOREIGN KEY ("nazwaLini_k")
-    REFERENCES public."Linia" (id_linii) MATCH SIMPLE
+    ADD CONSTRAINT nazwaLiniii_FK FOREIGN KEY (nazwaLini_k)
+    REFERENCES public.Linia (id_linii) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.kurs
-    ADD CONSTRAINT "rodzaj_bus_FK" FOREIGN KEY (rbus_k)
+    ADD CONSTRAINT rodzaj_bus_FK FOREIGN KEY (rbus_k)
     REFERENCES public.rodzaj_autobusu (id_rodzaj) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -187,7 +187,7 @@ ALTER TABLE IF EXISTS public.kurs
 
 
 ALTER TABLE IF EXISTS public.kurs_realizacja
-    ADD CONSTRAINT "bus_k_FK" FOREIGN KEY ("id_busuRel")
+    ADD CONSTRAINT bus_k_FK FOREIGN KEY (id_busuRel)
     REFERENCES public.autobusy (id_bus) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -195,7 +195,7 @@ ALTER TABLE IF EXISTS public.kurs_realizacja
 
 
 ALTER TABLE IF EXISTS public.kurs_realizacja
-    ADD CONSTRAINT "dzien_FK" FOREIGN KEY (dzien_rel)
+    ADD CONSTRAINT dzien_FK FOREIGN KEY (dzien_rel)
     REFERENCES public.dni_tygodnia (id_dnityg) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -203,39 +203,39 @@ ALTER TABLE IF EXISTS public.kurs_realizacja
 
 
 ALTER TABLE IF EXISTS public.kurs_realizacja
-    ADD CONSTRAINT "kierowcy_FK" FOREIGN KEY ("id_kierowcyRel")
-    REFERENCES public.dostepnosci_kierowcow ("id_dostKier") MATCH SIMPLE
+    ADD CONSTRAINT kierowcy_FK FOREIGN KEY (id_kierowcyRel)
+    REFERENCES public.dostepnosci_kierowcow (id_dostKier) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.kurs_realizacja
-    ADD CONSTRAINT "kurs_FK" FOREIGN KEY (id_kursu)
+    ADD CONSTRAINT kurs_FK FOREIGN KEY (id_kursu)
     REFERENCES public.kurs (id_kurs) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."przystanekwLinii"
-    ADD CONSTRAINT "linia_FK" FOREIGN KEY (linia)
-    REFERENCES public."Linia" (id_linii) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.przystanekwLinii
+    ADD CONSTRAINT linia_FK FOREIGN KEY (linia)
+    REFERENCES public.linia (id_linii) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."przystanekwLinii"
-    ADD CONSTRAINT "przystanke_FK" FOREIGN KEY (przystanek_id)
-    REFERENCES public."Przystanki" (id_przystanku) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.przystanekwLinii
+    ADD CONSTRAINT przystanek_FK FOREIGN KEY (przystanek_id)
+    REFERENCES public.przystanki (id_przystanku) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.raport
-    ADD CONSTRAINT "rozkla_FK" FOREIGN KEY (id_rozkl)
+    ADD CONSTRAINT rozklad_FK FOREIGN KEY (id_rozkl)
     REFERENCES public.rozklad_jazdy (id_rj) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -243,7 +243,7 @@ ALTER TABLE IF EXISTS public.raport
 
 
 ALTER TABLE IF EXISTS public.rozklad_jazdy
-    ADD CONSTRAINT "kurs_FK" FOREIGN KEY (nazwa_kursu_id)
+    ADD CONSTRAINT kurs_FK FOREIGN KEY (nazwa_kursu_id)
     REFERENCES public.kurs (id_kurs) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -251,16 +251,16 @@ ALTER TABLE IF EXISTS public.rozklad_jazdy
 
 
 ALTER TABLE IF EXISTS public.rozklad_jazdy
-    ADD CONSTRAINT "przystankiwlini_FK" FOREIGN KEY (id_przystankiwlini)
-    REFERENCES public."przystanekwLinii" ("id_przystLin") MATCH SIMPLE
+    ADD CONSTRAINT przystankiwlini_FK FOREIGN KEY (id_przystankiwlini)
+    REFERENCES public.przystanekwLinii (id_przystLin) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.uzytkownicy
-    ADD CONSTRAINT "id_stanowisko_FK" FOREIGN KEY (id_stanowisko)
-    REFERENCES public.typ_stanowiska ("id_typStanowiska") MATCH SIMPLE
+    ADD CONSTRAINT id_stanowisko_FK FOREIGN KEY (id_stanowisko)
+    REFERENCES public.typ_stanowiska (id_typStanowiska) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
