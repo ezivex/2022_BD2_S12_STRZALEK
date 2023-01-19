a19604cb37c78f5077d6f8a052d4e805e4993805
kamilpawlica <kamilpawlica99@gmail.com>
2023-01-19 11:39:41 +0100

    Update DispatcherEdycjaKursy.jsx

--- komunikacja-react/src/pages/DispatcherEdycjaKursy.jsx
+++ komunikacja-react/src/pages/DispatcherEdycjaKursy.jsx
@@ -47,7 +47,7 @@ const testyy2 = [];
 const getData = async () => {
   let result = items;
 result.map((user) => {
-  return testyy2.push({value: user.id_kurs, label: user.nazwalinii});
+  return testyy2.push({value: user.id_kurs, label: user.id_kurs + "." + user.nazwalinii});
 });
 };
 getData();
@@ -89,7 +89,71 @@ setTimeout(() => {
     callback(filteredOptions);
 })
 }
-//===============
+//=========================
+
+const [items1, setPrzystanek1] = useState([]);
+const getautobustyp1 = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/dni_tygodnia");
+    const jsonData = await response.json();
+
+    setPrzystanek1(jsonData);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+useEffect(() => {
+  getautobustyp1();
+}, []);
+const testyy21 = [];
+const getData1 = async () => {
+  let result = items1;
+result.map((user) => {
+  return testyy21.push({value: user.id_dnityg, label: user.dzien});
+});
+};
+getData1();
+console.log("testyy21");
+console.log(testyy21);
+const options1 = [];
+  
+ const xd1 = () => {
+  console.log("zapisywanie do options");
+  items1.forEach((element) => {
+    console.log(element.id_dnityg);
+    console.log(typeof element.id_dnityg);
+    console.log(typeof '${element.dzien}');
+    options1.push({
+        value: element.id_dnityg,
+        label: `${element.dzien}`,
+    });
+    console.log(options1);
+    console.log(options1[0]);
+    //return options;
+  });
+
+ };
+const handleChange1 = (selectedOption) => {
+//xd();
+console.log(selectedOption.value);
+setDzien(selectedOption.value);
+return selectedOption.value;
+};
+const loadOptions1 = (searchValue, callback) => {
+xd1();
+setTimeout(() => {
+    console.log("loadOptions");
+    console.log(options1);
+    console.log(testyy21);
+
+    const filteredOptions = options1.filter(option => option.label.includes(searchValue))
+    callback(filteredOptions);
+})
+}
+
+
+
+
 //=========================
 
     return (
@@ -117,7 +181,9 @@ setTimeout(() => {
                         </div>
                         <div className="input-container">
                             <label>Dzień </label>
-                            <input type="number" name="dzien" onChange={e => setDzien(e.target.value) } required />
+                            {/* <input type="number" name="dzien" onChange={e => setDzien(e.target.value) } required /> */}
+                            <CreatableSelect options={testyy21} defaultOptions onChange={handleChange1} />
+                            <input type="number" name="dzien" onChange={e => setDzien(e.target.selectedOption.value) } hidden />
                         </div>
                         <div className="input-container">
                             <label>Kierowca </label>
