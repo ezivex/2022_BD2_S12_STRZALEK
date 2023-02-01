f6d2cac1f7989f3c0e496f3140f4907d762b9eb1
lukaszwolny <lukawol372@student.polsl.pl>
2023-02-01 11:55:40 +0100

    dallinirozkladjazdy

--- komunikacja-react/src/components/TableCheckLinie.jsx
+++ komunikacja-react/src/components/TableCheckLinie.jsx
@@ -40,8 +40,76 @@ const getPrzystanekwLinii = async () => {
 useEffect(() => {
     getPrzystanekwLinii();
 }, []);
+//==========================================================================
 
+// const mojafun2 = async () => {
+//     let result3 = items;
+//     result3.map((ele) => {
+//         nazywkursrealziacja.push(ele.id_kursu);
+//         nazywkursrealziacjakierowcy.push(ele.id_kierowcyrel);
+//         nazywkursrealziacjabus.push(ele.id_busurel);
+//     });
+//     return nazywkursrealziacja;
+//     return nazywkursrealziacjakierowcy;
+//     return nazywkursrealziacjabus;
+//     console.log("nazywkursrealziacja");
+//     console.log(nazywkursrealziacja);
+//   };
+//   mojafun2();
+const daneprzysgodz = [];
+const danenazwaprzyst = [];
+const mojafun2 = async () => {
+    let result3 = items;
+    console.log("items:");
+    console.log(items);
+    let godzinyprzystankujednego = [];
+    result3.map((ele) => {
+        //zebranie wszystkich przystankow
+        danenazwaprzyst.push(ele.nazwaprzystanku);//wszystkie przystanki co sa w tej linii np 3x wszystkie bo 3 kursy ...
+        console.log("pppppppppppppppppppppppp");
+        console.log(ele.nazwaprzystanku);
+        console.log("pppppppppppppppppppppppp");
+        // if(ele.nazwaprzystanku === "Przystanek A"){
+        //     godzinyprzystankujednego.push(ele.godzina_odjazdu);
+        //     console.log(godzinyprzystankujednego);
+        // }
+    });
+    console.log("==");
+    console.log("--");
+    var nowa = [];
+    danenazwaprzyst.forEach(element => {
+        if(!nowa.includes(element))
+        {
+            nowa.push(element);
+        }
+    })
+    // danenazwaprzyst.filter((item,index) => danenazwaprzyst.indexOf(item) === index);
+    console.log(nowa);//same przystanki 
+    console.log("--");
+
+    //laczenie przyst z godz
+    nowa.forEach(element => {
+        result3.map((ele) => {
+            if(ele.nazwaprzystanku === element){
+                godzinyprzystankujednego.push(ele.godzina_odjazdu + "  |  ");
+                console.log(godzinyprzystankujednego);
+            }
+        
+        });
+        //teraz to zapisać do mapy już na stałe
+        daneprzysgodz.push({value: element, label:  godzinyprzystankujednego});
+        godzinyprzystankujednego = [];//reset
+    });
+        
+  };
+  mojafun2();
   
+console.log(daneprzysgodz);
+
+// const testytakei = [{
+//     value: 1,label: "okok",
+//     value: 2,label: "okaaaaok"
+// }];
  return (
 
       <div>
@@ -51,16 +119,24 @@ useEffect(() => {
 
                   <tr>
                       <th>Przystanki w linii</th>
-                      <th>Godzina</th>
+                      <th>Godziny</th>
                   </tr>
 
               </thead>
 
-              <tbody>
+              {/* <tbody>
                   {items.map(item => (
                       <tr key={item.id_rj}>
                           <td>{item.nazwaprzystanku}</td>
-                          <td>Godzina</td>
+                          <td>{item.godzina_odjazdu}</td>
+                      </tr>
+                  ))}
+              </tbody> */}
+              <tbody>
+                  {daneprzysgodz.map(item => (
+                      <tr key={item.value}>
+                          <td>{item.value}</td>
+                          <td>{item.label}</td>
                       </tr>
                   ))}
               </tbody>
