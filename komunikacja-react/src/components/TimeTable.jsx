26a9e537e3feac648a92144e226328ec977f1412
lukaszwolny <lukawol372@student.polsl.pl>
2023-02-04 20:53:34 +0100

    jestembliskoprawietomam

--- komunikacja-react/src/components/TimeTable.jsx
+++ komunikacja-react/src/components/TimeTable.jsx
@@ -73,38 +73,104 @@ const generatePDF = async () => {
         
 //   };
 //   mojafun2();
+const kursyall = [];
 const wszystkiekursyy = [];
+const xdxdxd = [];
+const przystaneczkikochane = [];
 const mojafun2 = async () => {
     let result3 = schedule;
     console.log("items:");
     console.log(schedule);
     let godzinyprzystankujednego = [];
+    var nowa = [];
     result3.map((ele) => {
-        //zebranie wszystkich przystankow
-        if (!wszystkiekursyy.some(x => x.id_przystankiwlini === ele.id_przystankiwlini))
-        {
-            wszystkiekursyy.push(ele);//wszystkie przystanki co sa w tej linii np 3x wszystkie bo 3 kursy ...
-        }
+        //zebranie wszystkich przystankow / kursów / linie
+        if(!wszystkiekursyy.includes(ele.nazwalini_k))
+            {
+                wszystkiekursyy.push(ele.nazwalini_k);
+            }
+        // if (!wszystkiekursyy.some(x => x.nazwa_kursu_id === ele.nazwa_kursu_id))
+        // {
+        //     wszystkiekursyy.push(ele);//wszystkie przystanki co sa w tej linii np 3x wszystkie bo 3 kursy ...
+        // }
+        // if (!wszystkiekursyy.some(x => x.id_przystankiwlini === ele.id_przystankiwlini))
+        // {
+        //     wszystkiekursyy.push(ele);//wszystkie przystanki co sa w tej linii np 3x wszystkie bo 3 kursy ...
+        // }
         
         console.log("pppppppppppppppppppppppp");
-        console.log(ele);
+        console.log(wszystkiekursyy);
+        // const kursyall = [];
+        // wszystkiekursyy.forEach(element => {//wszystkie kursy to sa wszystkie linie tak naprawde jakei sa 
+        //     // if(!kursyall.includes(element.nazwalini_k))
+        //     // {
+        //     //     // w ifie daje godziny a potem to lacze
+        //     //     kursyall.push({value: element.nazwalini_k, label: element.nazwalinii});
+        //     // }
+        // });
+        console.log(kursyall);//wsyzstkie linie jakie sa
         console.log("pppppppppppppppppppppppp");
         // if(ele.nazwaprzystanku === "Przystanek A"){
         //     godzinyprzystankujednego.push(ele.godzina_odjazdu);
         //     console.log(godzinyprzystankujednego);
         // }
+
+
+        //nazyw przystankow biore
+        wszystkiekursyy.forEach(element => {
+            if(ele.id_linii === element){
+            {
+                if(!nowa.includes(ele.id_przystankiwlini))
+                    nowa.push(ele.id_przystankiwlini);
+                    //console.log(godzinyprzystankujednego);
+                }
+                //nowa.push(element);
+                console.log("opopop" + nowa);
+            }
+            
+            //przystaneczkikochane.push({value: element, label:  nowa});
+            //nowa = [];//reset
+    })
+    
+
+        return kursyall;
     });
+
+    console.log("opopop koniec");
+
+
+    //teraz do kazdego lini id daje nazwe tej lini
+    let nazywkursunormalnie = [];
+    wszystkiekursyy.forEach(element => {
+        result3.map((ele) => {
+            if(ele.nazwalini_k === element){
+                if(!nazywkursunormalnie.includes(ele.nazwalinii))
+                {
+                    nazywkursunormalnie.push(ele.nazwalinii);
+                }
+            }
+        });
+        //teraz to zapisać do mapy już na stałe
+        xdxdxd.push({value: element, label:  nazywkursunormalnie, co: "siemani co tam"});
+        nazywkursunormalnie = [];//reset
+    });
+
+
+    //====
     console.log("==");
     console.log("--");
-    var nowa = [];
-    wszystkiekursyy.forEach(element => {
-        if(!nowa.includes(element))
-        {
-            nowa.push(element);
-        }
+    // var nowa = [];
+    // wszystkiekursyy.forEach(element => {
+    //     if(!nowa.includes(element))
+    //     {
+    //         nowa.push(element);
+    //     }
         
-    })
+    // })
     console.log(nowa);//same przystanki
+console.log("====");
+console.log("xdxdxd:");
+console.log(xdxdxd);
 console.log("====");
 };
 mojafun2();
@@ -118,15 +184,21 @@ return (
       <div className="schedule-container" ref={printRef}>
         {schedule.reduce((unique, item) => {
             
-            unique.push(item);
+            //unique.push(item);
+            unique = xdxdxd;
             // if (!unique.some(x => x.id_przystankiwlini === item.id_przystankiwlini)) {
             //     unique.push(item);
             // }
-            return unique;
+         
+            return unique;//to jest shedule
         }, []).map((item, index) => (
             <div key={item.id_rj}>
-                {(index === 0 || item.nazwa_kursu_id !== schedule[index - 1].nazwa_kursu_id) && (
-                    <h2 className="schedule-title">Nazwa kursu: {item.nazwalinii}</h2>
+                {(index === 0 || item.value) && (
+                    // xdxdxd.map((ele) => {
+                    //     <h2 className="schedule-title">Nazwa kursu: {ele.value}</h2>
+                    // })                
+                    <h2 className="schedule-title">Nazwa kursu: {item.label}</h2>
+                    //wyswietlaja sie wszystkie kursy jakie sa git.
                 )}
 
                 <div className="schedule-details-container">
@@ -135,7 +207,13 @@ return (
                         {item.nazwaprzystanku}
                         </div>
                         <div className="schedule-departure-time">Godziny odjazdów: 
+                        {/* godziny: */}
                         <p className="schedule-departure-text">{(schedule.filter(x => x.nazwa_kursu_id === item.nazwa_kursu_id && x.id_przystankiwlini === item.id_przystankiwlini).length > 1 ? schedule.filter(x => x.nazwa_kursu_id === item.nazwa_kursu_id && x.id_przystankiwlini === item.id_przystankiwlini).map(x => x.godzina_odjazdu).join(', ') : item.godzina_odjazdu)}</p>
+                        <p className="schedule-departure-text">da</p>
+                        <p className="schedule-departure-text">da</p>
+                        <p className="schedule-departure-text">da</p>
+                        <p className="schedule-departure-text">da</p>
+                        {/* <p className="schedule-departure-text">{(schedule.filter(x => x.nazwa_kursu_id === item.nazwa_kursu_id && x.id_przystankiwlini === item.id_przystankiwlini).length > 1 ? schedule.filter(x => x.nazwa_kursu_id === item.nazwa_kursu_id && x.id_przystankiwlini === item.id_przystankiwlini).map(x => x.godzina_odjazdu).join(', ') : item.godzina_odjazdu)}</p> */}
                         </div>
                     </div>
                 </div>
